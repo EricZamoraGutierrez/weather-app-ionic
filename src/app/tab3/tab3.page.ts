27ce111110d36322b038085969c394578e3e2a9b
Gunter <eric.zamora@gmail.com>
2023-06-14 19:23:39 -0500

    Theme color progress

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,34 @@
 import { Component } from '@angular/core';
+import { ThemesService } from '../services/themes.service';
+
+
+const themes:any = {
+  autumn: {
+    primary: '#F78154',
+    secondary: '#4D9078',
+    tertiary: '#B4436C',
+    light: '#FDE8DF',
+    medium: '#FCD0A2',
+    dark: '#B89876'
+  },
+  night: {
+    primary: '#8CBA80',
+    secondary: '#FCFF6C',
+    tertiary: '#FE5F55',
+    medium: '#BCC2C7',
+    dark: '#F7F7FF',
+    light: '#495867'
+  },
+  neon: {
+    primary: '#39BFBD',
+    secondary: '#4CE0B3',
+    tertiary: '#FF5E79',
+    light: '#F4EDF2',
+    medium: '#B682A5',
+    dark: '#34162A'
+  }
+};
+
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +37,9 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private theme: ThemesService) {}
 
+  changeTheme(name:any) {
+    this.theme.setTheme(themes[name]);
+  }
 }
