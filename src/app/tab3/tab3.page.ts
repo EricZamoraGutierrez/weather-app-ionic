aa063f9d3f62272f666ccc24d08fd93724cf3ae7
Gunter <eric.zamora@gmail.com>
2023-06-16 16:16:49 -0500

    Buttons Fixed

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,33 +2,6 @@ import { Component } from '@angular/core';
 import { ThemesService } from '../services/themes.service';
 
 
-const themes:any = {
-  autumn: {
-    primary: '#F78154',
-    secondary: '#4D9078',
-    tertiary: '#B4436C',
-    light: '#FDE8DF',
-    medium: '#FCD0A2',
-    dark: '#B89876'
-  },
-  night: {
-    primary: '#8CBA80',
-    secondary: '#FCFF6C',
-    tertiary: '#FE5F55',
-    medium: '#BCC2C7',
-    dark: '#F7F7FF',
-    light: '#495867'
-  },
-  neon: {
-    primary: '#39BFBD',
-    secondary: '#4CE0B3',
-    tertiary: '#FF5E79',
-    light: '#F4EDF2',
-    medium: '#B682A5',
-    dark: '#34162A'
-  }
-};
-
 
 @Component({
   selector: 'app-tab3',
@@ -36,10 +9,30 @@ const themes:any = {
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  buttonText: string = 'Add';
 
-  constructor(private theme: ThemesService) {}
 
-  changeTheme(name:any) {
-    this.theme.setTheme(themes[name]);
+  constructor() { }
+
+  ThemeSelector(theme: any) {
+    let body = document.getElementsByTagName('html')[0];
+
+    if (this.buttonText === 'Add') {
+      this.buttonText = 'Remove';
+      switch (theme) {
+        case 'zerotwo':
+          body.classList.remove();
+          body.classList.add('md-ZeroTwo');
+          break;
+        case 'shinobu':
+          body.classList.remove();
+          body.classList.add('md-Shinobu');
+      }
+    } else {
+      this.buttonText = 'Add';
+      body.classList.remove('md-ZeroTwo');
+      body.classList.remove('md-Shinobu');
+      body.classList.add('md');
+    }
   }
 }
