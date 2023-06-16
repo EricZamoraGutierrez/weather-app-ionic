e1999ebde4cc94e5b7cc86f754ead5c6fb03cecf
Benjamin Webster <2102183@utrivieramaya.edu.mx>
2023-06-16 18:52:54 -0500

    hpihjo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
-    path: 'tab4',
-    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
-  }
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    data: {
+      animated: true,
+      animationDirection: 'forward'
+    }
+  },
+ 
 
 ];
 @NgModule({
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,25 +1,25 @@
 import { Component } from '@angular/core';
 import { ThemesService } from '../services/themes.service';
 
-
-
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
   toggler: string = 'Add';
 
-
-  constructor() { }
+  constructor() {}
 
   ThemeSelector(theme: any) {
     let body = document.getElementsByTagName('html')[0];
     body.classList.remove('md-ZeroTwo');
     body.classList.remove('md-Shinobu');
+    body.classList.remove('md-rem');
+    body.classList.remove('md-tatsumaki');
+    body.classList.remove('md-marin');
 
-    if (theme == 'zerotwo' || theme == 'shinobu') {
+    if (theme == 'zerotwo' || theme == 'shinobu' || theme == 'rem' || theme == 'tatsumaki' || theme == 'marin') {
       switch (theme) {
         case 'zerotwo':
           body.classList.remove();
@@ -29,15 +29,25 @@ export class Tab3Page {
           body.classList.remove();
           body.classList.add('md-Shinobu');
           break;
-          case 'rem':
+        case 'rem':
           body.classList.remove();
           body.classList.add('md-rem');
           break;
+          case 'tatsumaki':
+            body.classList.remove();
+            body.classList.add('md-tatsumaki');
+            break;  
+            case 'marin':
+            body.classList.remove();
+            body.classList.add('md-marin');
+            break; 
       }
     } else {
       body.classList.remove('md-ZeroTwo');
       body.classList.remove('md-Shinobu');
       body.classList.remove('md-rem');
+      body.classList.remove('md-tatsumaki');
+      body.classList.remove('md-marin');
       body.classList.add('md');
     }
   }
