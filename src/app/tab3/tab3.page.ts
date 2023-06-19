4c5e6cd258da6dd8d66e60555fd278911a9ef25e
Gunter <eric.zamora@gmail.com>
2023-06-18 23:11:32 -0500

    Solved Theme Storage

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ThemesService } from '../services/themes.service';
-import { Preferences } from '@capacitor/preferences';
 
 
 @Component({
@@ -9,71 +8,16 @@ import { Preferences } from '@capacitor/preferences';
   styleUrls: ['tab3.page.scss'],
 })
 
-export class Tab3Page implements OnInit{
+export class Tab3Page{
 
-  ngOnInit() {
-    this.getTheme();  
-  }
+  
 
   toggler: string = 'Add';
 
 
+  constructor(private themeSelector: ThemesService) { }
 
-  constructor() { }
-
-  ThemeSelector(theme: any) {
-    let body = document.getElementsByTagName('html')[0];
-    body.classList.remove('md-ZeroTwo');
-    body.classList.remove('md-Shinobu');
-    body.classList.remove('md-rem');
-    body.classList.remove('md-tatsumaki');
-    body.classList.remove('md-marin');
-
-    if (theme == 'zerotwo' || theme == 'shinobu' || theme == 'rem' || theme == 'tatsumaki' || theme == 'marin') {
-      switch (theme) {
-        case 'zerotwo':
-          body.classList.remove();
-          body.classList.add('md-ZeroTwo');
-          this.setTheme('zerotwo');
-          break;
-        case 'shinobu':
-          body.classList.remove();
-          body.classList.add('md-Shinobu');
-          this.setTheme('shinobu');
-          break;
-        case 'rem':
-          body.classList.remove();
-          body.classList.add('md-rem');
-          this.setTheme('rem');
-          break;
-        case 'tatsumaki':
-          body.classList.remove();
-          body.classList.add('md-tatsumaki');
-          this.setTheme('tatsumaki');
-          break;
-        case 'marin':
-          body.classList.remove();
-          body.classList.add('md-marin');
-          this.setTheme('marin');
-          break;
-      }
-    } else {
-      body.classList.remove('md-ZeroTwo');
-      body.classList.remove('md-Shinobu');
-      body.classList.remove('md-rem');
-      body.classList.remove('md-tatsumaki');
-      body.classList.remove('md-marin');
-      body.classList.add('md');
-    }
-  }
-
-  async setTheme(theme: any) {
-    await Preferences.set({ key: 'theme', value: theme });
-    console.log('Theme set to: ' + theme);
-  }
-
-  async getTheme() {
-    const { value } = await Preferences.get({ key: 'theme' });
-    this.ThemeSelector(value);
+  ThemeSelector(Theme: any) {
+    this.themeSelector.ThemeSelector(Theme);
   }
 }
