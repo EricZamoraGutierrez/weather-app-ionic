e943586d106e39b8b5ffe188d02d92e8bc7c96de
Gunter <eric.zamora@gmail.com>
2023-06-18 22:54:57 -0500

    Solving capacitor storage

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ThemesService } from '../services/themes.service';
+import { Preferences } from '@capacitor/preferences';
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
 })
-export class Tab3Page {
+
+export class Tab3Page implements OnInit{
+
+  ngOnInit() {
+    this.getTheme();  
+  }
+
   toggler: string = 'Add';
 
-  constructor() {}
+
+
+  constructor() { }
 
   ThemeSelector(theme: any) {
     let body = document.getElementsByTagName('html')[0];
@@ -24,23 +34,28 @@ export class Tab3Page {
         case 'zerotwo':
           body.classList.remove();
           body.classList.add('md-ZeroTwo');
+          this.setTheme('zerotwo');
           break;
         case 'shinobu':
           body.classList.remove();
           body.classList.add('md-Shinobu');
+          this.setTheme('shinobu');
           break;
         case 'rem':
           body.classList.remove();
           body.classList.add('md-rem');
+          this.setTheme('rem');
           break;
-          case 'tatsumaki':
-            body.classList.remove();
-            body.classList.add('md-tatsumaki');
-            break;  
-            case 'marin':
-            body.classList.remove();
-            body.classList.add('md-marin');
-            break; 
+        case 'tatsumaki':
+          body.classList.remove();
+          body.classList.add('md-tatsumaki');
+          this.setTheme('tatsumaki');
+          break;
+        case 'marin':
+          body.classList.remove();
+          body.classList.add('md-marin');
+          this.setTheme('marin');
+          break;
       }
     } else {
       body.classList.remove('md-ZeroTwo');
@@ -50,5 +65,15 @@ export class Tab3Page {
       body.classList.remove('md-marin');
       body.classList.add('md');
     }
+  }
+
+  async setTheme(theme: any) {
+    await Preferences.set({ key: 'theme', value: theme });
+    console.log('Theme set to: ' + theme);
+  }
+
+  async getTheme() {
+    const { value } = await Preferences.get({ key: 'theme' });
+    this.ThemeSelector(value);
   }
 }
