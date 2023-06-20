55ca5b1745a5c045a6c4d6018ac036a65fd10267
Gunter <eric.zamora@gmail.com>
2023-06-20 15:14:18 -0500

    Working on Weather

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,17 +1,18 @@
 import { AnimationBuilder } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-
+import { Geolocation } from '@capacitor/geolocation';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page  {
+  
 
-  constructor() {}
-
+  constructor() { }
 
 
 }
