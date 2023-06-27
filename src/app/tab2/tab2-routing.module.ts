5f7093e8d1cc9c0fc170b3bf3925e20a612c3a7f
Gunter <eric.zamora@gmail.com>
2023-06-26 19:19:52 -0500

    Bendiciones

--- src/app/tab2/tab2-routing.module.ts
+++ src/app/tab2/tab2-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab2Page } from './tab2.page';
+import { ModalController } from '@ionic/angular';
+import { RoutineSelectorPage } from './routine-selector/routine-selector.page';
 
 const routes: Routes = [
   {
     path: '',
     component: Tab2Page,
+  },
+  {
+    path: 'routine-selector',
+    loadChildren: () => import('./routine-selector/routine-selector.module').then(m => m.RoutineSelectorPageModule)
   }
 ];
 
@@ -13,4 +19,7 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class Tab2PageRoutingModule {}
+export class Tab2PageRoutingModule {
+
+
+}
