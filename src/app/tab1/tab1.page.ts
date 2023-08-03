3c3d5a1bd74eb6c9b29c939efb28800144d51861
Gunter <eric.zamora@gmail.com>
2023-08-02 19:06:20 -0500

    Background changes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Geolocation } from '@capacitor/geolocation';
 import { HttpService } from '../services/http.service';
+import { ThemesService } from '../services/themes.service';
 
 @Component({
   selector: 'app-tab1',
@@ -12,7 +13,28 @@ import { HttpService } from '../services/http.service';
 export class Tab1Page  {
   
 
-  constructor() { }
+  constructor(private theme: ThemesService) { }
+
+  show: boolean = false;
+
+  themeName: any = "";
 
 
+
+  async themename(){
+   this.themeName = await this.theme.getTheme();
+  }
+
+  async checkImg(){
+    if( this.themeName == "zerotwo" ){
+      this.show = false; 
+    }else{
+      this.show = true;
+    } 
+  }
+
+  readTask =  setInterval(() => {
+    this.themename();
+    this.checkImg();
+  }, 2000);
 }
